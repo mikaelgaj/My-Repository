a54e606951368bca9ae2e6b58bc14a3d1d8e3a7a
mikaelgaj <mikael7gaj@yahoo.co.uk>
2016-10-22 17:48:58 +0100

    Update Python Addition.py
    Making the code add 3 numbers instead of 2

--- Python Addition.py
+++ Python Addition.py
@@ -4,6 +4,8 @@ number1=float(input("Please enter a number"))
 
 number2=float(input("Please enter the number you would like to add"))
 
-sum=float(number1) + float(number2)
+number3=float(input("Please enter the second number you would like to add"))
+
+sum=float(number1) + float(number2) + float(number3)
 
 print(sum)
