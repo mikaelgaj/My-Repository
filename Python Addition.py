4b4c0646593d356e7c1199544fcb0ce416ef67f5
mikaelgaj <mikael7gaj@yahoo.co.uk>
2016-10-22 14:58:33 +0100

    Add files via upload
    This is a basic addition code on python. It will tell you to enter a number, then ask for the number you want to be added, and calculate this sum for you.

---
+++ Python Addition.py
@@ -0,0 +1,9 @@
+#basic python addition code
+
+number1=float(input("Please enter a number"))
+
+number2=float(input("Please enter the number you would like to add"))
+
+sum=float(number1) + float(number2)
+
+print(sum)
