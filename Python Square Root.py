17f0f39e0ec3478e0e6bf0edf8024861980b8c33
mikaelgaj <mikael7gaj@yahoo.co.uk>
2016-10-23 15:07:33 +0100

    Update Python Square Root.py

--- Python Square Root.py
+++ Python Square Root.py
@@ -1,5 +1,5 @@
 # Python Program to calculate the square root
 
-num = float(input("Enter a number: "))
+num = float(input("Enter a number: ")) #change this number to calculate the root.
 num_sqrt = num ** 0.5
 print("The square root of %0.3f is %0.3f"%(num ,num_sqrt))
